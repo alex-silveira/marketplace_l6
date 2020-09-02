3f656a82c66dbd5ba1d8e74f48c4a71a02371579
alex <alexarauj0015@hotmail.com>
2020-09-02 10:20:24 -0300

    Queries eloquent

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ Route::get('/', function () {
     //return view('welcome', ['helloWorld' => $helloWorld]);
     return view('welcome', compact('helloWorld'));
 });
+
+Route::get('/model', function(){
+    //$products = \App\Product::all(); //select * from products
+
+    //$user = new \App\User();
+    //$user = \App\User::find(1);
+    //$user->name = 'Usuário Teste editado';
+
+    //$user->save();
+
+    //\App\User::all(); - retorna todos os usuários
+    //\App\User::find(3); - retorna o usuário com base no id
+    //\App\User::where('name', 'Dusty Rempel')->get(); - select * from users where name = 'Dusty Rempel'
+    //\App\User::where('name', 'Dusty Rempel')->first(); - primeiro resultado do select
+    //\App\User::paginate(10); - paginar dados
+    
+    //return $products;
+});
