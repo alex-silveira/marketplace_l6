b1ad4d46f935d0400b6416808c2b8d64a6446c3e
alex <alexarauj0015@hotmail.com>
2020-09-15 19:34:08 -0300

    Add laravel ui auth

--- routes/web.php
+++ routes/web.php
@@ -141,3 +141,6 @@ Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function(){
     Route::resource('stores', 'StoreController');
     Route::resource('products', 'ProductController');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
