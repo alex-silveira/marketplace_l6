8e1678914d47af163671b9d9578820faed215c9a
D:\Laravel <alexarauj0015@hotmail.com>
2020-09-21 20:54:55 -0300

    Add category list

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -25,6 +25,17 @@
         <label for="">Preço</label>
         <input type="text" name="price" class="form-control" value="{{$product->price}}">
     </div>
+  
+    <div class="form-group">
+        <label for="">Categorias</label>
+        <select name="categories[]" id="" class="form-control" multiple>
+            @foreach ($categories as $category)
+                <option value="{{$category->id}}"
+                    @if ($product->categories->contains($category)) selected @endif                        
+                >{{$category->name}}</option>
+            @endforeach
+        </select>
+    </div>
 
     <div class="form-group">
         <label for="">Slug</label>
