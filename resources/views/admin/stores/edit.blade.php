063022a7f1a5482ecfb2a188b7bf6baa3daa99c4
Alex <alexarauj0015@hotmail.com>
2020-09-29 10:36:17 -0300

    Add field for edit logo

--- resources/views/admin/stores/edit.blade.php
+++ resources/views/admin/stores/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <h1>Editar Loja</h1>
-    <form action="{{route('admin.stores.update', ['store' => $store->id])}}" method="post">
+    <form action="{{route('admin.stores.update', ['store' => $store->id])}}" method="post" enctype="multipart/form-data">
         @csrf
         @method("PUT")
         
@@ -25,6 +25,20 @@
             <input type="text" name="mobile_phone" class="form-control" value="{{$store->mobile_phone}}">
         </div>
         
+        <div class="form-group">
+            <p>
+                <img src="{{asset('storage/'.$store->logo)}}" alt="">
+            </p>
+            <label>Fotos do produto</label>
+            <input type="file" name="logo" class="form-control @error('logo') is-invalid @enderror" multiple>
+
+            @error('logo') 
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
+        </div>
+
         <div class="form-group">
             <label for="">Slug</label>
             <input type="text" name="slug" class="form-control" value="{{$store->slug}}">
