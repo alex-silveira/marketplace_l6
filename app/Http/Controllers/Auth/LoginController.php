c5b9e2a785a0907b7780d361dfe6a83b75be883c
Alex <alexarauj0015@hotmail.com>
2020-10-10 19:36:40 -0300

    Changed: parameter name error

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -38,7 +38,7 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    protected function authenticated(Request $resquest, $user)
+    protected function authenticated(Request $request, $user)
     {
         if(session()->has('cart')){
             return redirect()->action('checkout.index');
